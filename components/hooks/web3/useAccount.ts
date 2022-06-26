0c484d2958e673c01c9f4441a84050a9112a6296
risingcore <vcjsm2283@gmail.com>
2022-06-26 23:01:19 +0900

    update

--- components/hooks/web3/useAccount.ts
+++ components/hooks/web3/useAccount.ts
@@ -1,14 +1,21 @@
 import { CryptoHookFactory } from '@_types/hooks';
+import { useEffect } from 'react';
 import useSWR from 'swr';
 
-type AccountHookFactory = CryptoHookFactory<string>;
+type UseAccountResponse = {
+  connect: () => void;
+  isLoading: boolean;
+  isInstalled: boolean;
+};
+
+type AccountHookFactory = CryptoHookFactory<string, UseAccountResponse>;
 
 export type UseAccountHook = ReturnType<AccountHookFactory>;
 
 export const hookFactory: AccountHookFactory =
-  ({ provider, ethereum }) =>
+  ({ provider, ethereum, isLoading }) =>
   () => {
-    const swrRes = useSWR(
+    const { data, mutate, isValidating, ...swr } = useSWR(
       provider ? 'web3/useAccount' : null,
       async () => {
         const accounts = await provider!.listAccounts();
@@ -24,7 +31,23 @@ export const hookFactory: AccountHookFactory =
         revalidateOnFocus: false,
       }
     );
-    // connect metamask
+
+    useEffect(() => {
+      ethereum?.on('accountsChanged', handleAccountsChanged);
+      return () => {
+        ethereum?.removeListener('accountsChanged', handleAccountsChanged);
+      };
+    });
+
+    const handleAccountsChanged = (...args: unknown[]) => {
+      const accounts = args[0] as string[];
+      if (accounts.length === 0) {
+        console.error('Please, connect to Web3 wallet');
+      } else if (accounts[0] !== data) {
+        mutate(accounts[0]);
+      }
+    };
+
     const connect = async () => {
       try {
         ethereum?.request({ method: 'eth_requestAccounts' });
@@ -34,7 +57,12 @@ export const hookFactory: AccountHookFactory =
     };
 
     return {
-      ...swrRes,
+      ...swr,
+      data,
+      isValidating,
+      isLoading: isLoading || isValidating,
+      isInstalled: ethereum?.isMetaMask || false,
+      mutate,
       connect,
     };
   };
