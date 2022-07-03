3ce1f617528c7b48b906332318851a47ce36195f
risingcore <vcjsm2283@gmail.com>
2022-07-03 10:58:09 +0900

    Get Nft Data

--- types/nft.ts
+++ types/nft.ts
@@ -1,14 +1,23 @@
-
-export type Trait = "attack" | "health" | "speed";
+export type Trait = 'attack' | 'health' | 'speed';
 
 export type NftAttribute = {
   trait_type: Trait;
   value: string;
-}
+};
 
 export type NftMeta = {
   name: string;
   description: string;
   image: string;
   attributes: NftAttribute[];
-}
+};
+export type NftCore = {
+  tokenId: number;
+  price: number;
+  creator: string;
+  isListed: boolean;
+};
+
+export type Nft = {
+  meta: NftMeta;
+} & NftCore;
