d50723cb80da07c4f3507107038fac620e856a2b
risingcore <vcjsm2283@gmail.com>
2022-07-04 22:11:01 +0900

    Use Callback

--- components/hooks/web3/useListedNfts.ts
+++ components/hooks/web3/useListedNfts.ts
@@ -1,6 +1,7 @@
 import { CryptoHookFactory } from '@_types/hooks';
 import { Nft } from '@_types/nft';
 import { ethers } from 'ethers';
+import { useCallback } from 'react';
 import useSWR from 'swr';
 
 type UseListedNftsResponse = {
@@ -35,17 +36,21 @@ export const hookFactory: ListedNftsHookFactory =
       return nfts;
     });
 
-    const buyNft = async (tokenId: number, value: number) => {
-      try {
-        const result = await contract?.buyNft(tokenId, {
-          value: ethers.utils.parseEther(value.toString()),
-        });
-        await result?.wait();
-        alert('You have bought Nft. See profile page.');
-      } catch (e: any) {
-        console.error(e.message);
-      }
-    };
+    const _contract = contract;
+    const buyNft = useCallback(
+      async (tokenId: number, value: number) => {
+        try {
+          const result = await _contract!.buyNft(tokenId, {
+            value: ethers.utils.parseEther(value.toString()),
+          });
+          await result?.wait();
+          alert('You have bought Nft. See profile page.');
+        } catch (e: any) {
+          console.error(e.message);
+        }
+      },
+      [_contract]
+    );
 
     return {
       ...swr,
