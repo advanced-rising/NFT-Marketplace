171307bd93b079bc0e06a74ba5aa810defb0ab14
risingcore <vcjsm2283@gmail.com>
2022-07-07 21:41:00 +0900

    Handle rest of promises

--- components/hooks/web3/useOwnedNfts.ts
+++ components/hooks/web3/useOwnedNfts.ts
@@ -2,6 +2,7 @@ import { CryptoHookFactory } from '@_types/hooks';
 import { Nft } from '@_types/nft';
 import { ethers } from 'ethers';
 import { useCallback } from 'react';
+import { toast } from 'react-toastify';
 import useSWR from 'swr';
 
 type UseOwnedNftsResponse = {
@@ -48,8 +49,11 @@ export const hookFactory: OwnedNftsHookFactory =
             }
           );
 
-          await result?.wait();
-          alert('Item has been listed!');
+          await toast.promise(result!.wait(), {
+            pending: 'Processing transaction',
+            success: 'Item has been listed',
+            error: 'Processing error',
+          });
         } catch (e: any) {
           console.error(e.message);
         }
